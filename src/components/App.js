833b1e848fc246a81b01a46de731b8b405f8f62a
bricejokung <bjokung@gmail.com>
2021-10-27 10:48:41 -0400

    add navigation and leaderboard data

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import GameBoard from './GameBoard';
 import GameList from './GameList';
+import Leaderboard from './Leaderboard';
+import NavBar from './NavBar';
 
 function App() {
+
   return (
     <div>
+     {/* add a Nav Bar here */}
+      < NavBar />
       <Switch>
-        {/* vvv Include routes for player pages here vvv */}
+        {/* vvv Include routes for player pages here vvv. the path will be path= '/players/new */}
 
 
 
@@ -20,7 +25,8 @@ function App() {
         </Route>
 
         <Route path='/'>
-          {/* Insert home page component(s) here */}
+          {/* Insert home page component(s) here   use LeaderBoard.js here */}
+          <Leaderboard/>
         </Route>
       </Switch>
     </div>
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import GameList from './GameList';
+import NewGameForm from './NewGameForm';
+import NewPlayerForm from './NewPlayerForm';
+import PlayerScore from './PlayerScore';
 
 function Leaderboard() {
+
+    const [playerlist, setPlayerlist] = useState([])
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}/players`)
+            .then(response => response.json())
+            .then(data => setPlayerlist(data))
+    }, [])
+    
+    const LeaderBoardRow = playerlist.map(playerObj=> (
+     
+        <div key = {playerObj.id}>
+        {playerObj.name}
+        {playerObj.winnings}
+        </div>
+
+    ))
     return (
         <div>
-            
+     {LeaderBoardRow}
+    
+
         </div>
     );
 }
 
 export default Leaderboard;
+// Fetch request to get the player on this URL: fetch(`${process.env.REACT_APP_API_URL}/players`) and render all players. Use State
+// Sort them by winnings ???
+// render them on the page
